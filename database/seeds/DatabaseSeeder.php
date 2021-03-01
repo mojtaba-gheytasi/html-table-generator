2514e5fff71f1bd63469fb7dea1563011a6ef31e
mojtaba gheytasi <mjgheytasi@gmail.com>
2021-03-02 02:04:46 +0330

    Changes in DatabaseSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,13 +5,8 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        factory(User::class,90)->create();
+        factory(User::class,80)->create();
     }
 }
